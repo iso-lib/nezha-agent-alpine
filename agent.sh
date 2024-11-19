f4cf30a36d7c4df09b07f95511e0f2a035654d61
iso-lib <77186879+iso-lib@users.noreply.github.com>
2024-11-19 23:57:30 +0800

    Update agent.sh

--- agent.sh
+++ agent.sh
@@ -125,7 +125,12 @@ echo -e "${green}开始尝试降级Agent"
             wget https://github.com/nezhahq/agent/releases/download/v0.${VERSION}/nezha-agent_linux_arm64.zip && unzip nezha-agent_linux_arm64.zip && rm nezha-agent_linux_arm64.zip && mv nezha-agent /opt/nezha/agent/nezha-agent
         fi
     else
-       echo -e "${yellow}/opt/nezha/agent/nezha-agent不存在,请尝试手动降级${plain}"
+       echo -e "${yellow}/opt/nezha/agent/nezha-agent不存在,准备下载v0.${VERSION}版${plain}"
+        if [[ "${arch}" == "amd64" ]]; then
+            wget https://github.com/nezhahq/agent/releases/download/v0.${VERSION}/nezha-agent_linux_amd64.zip && unzip nezha-agent_linux_amd64.zip && rm nezha-agent_linux_amd64.zip && mv nezha-agent /opt/nezha/agent/nezha-agent
+        elif [[ "${arch}" == "arm64" ]]; then
+            wget https://github.com/nezhahq/agent/releases/download/v0.${VERSION}/nezha-agent_linux_arm64.zip && unzip nezha-agent_linux_arm64.zip && rm nezha-agent_linux_arm64.zip && mv nezha-agent /opt/nezha/agent/nezha-agent
+        fi
     fi
 }
 
